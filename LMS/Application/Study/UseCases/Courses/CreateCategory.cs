fc94cc357365bb7f76f806c89a9bb4a6990627e0
pikoUsername <galymzhan.amantaj@gmail.com>
2024-05-29 13:42:20 +0500

    Added some use cases

--- LMS/Application/Study/UseCases/Courses/CreateCategory.cs
+++ LMS/Application/Study/UseCases/Courses/CreateCategory.cs
@@ -4,29 +4,43 @@ using LMS.Application.Study.Dto;
 using LMS.Application.Study.Interfaces;
 using LMS.Domain.Study.Entities;
 using Microsoft.AspNetCore.Components.Forms;
+using Newtonsoft.Json;
 
 namespace LMS.Application.Study.UseCases.Courses
 {
     public class CreateCategory : BaseUseCase<CreateCategoryDto, CategoryEntity>
     {
         private IApplicationDbContext _context { get; }
-        private IInstitutionAccessPolicy _institutionPolicy { get; }
         private IAccessPolicy _accessPolicy { get; }
 
 
         public CreateCategory(
             IApplicationDbContext dbContext,
-            IInstitutionAccessPolicy institutionPolicy,
             IAccessPolicy accessPolicy)
         {
             _accessPolicy = accessPolicy;
             _context = dbContext;
-            _institutionPolicy = institutionPolicy;
         }
 
         public async Task<CategoryEntity> Execute(CreateCategoryDto dto)
         {
-            return;
+            await _accessPolicy.EnforceIsAllowed(Domain.User.Enums.PermissionEnum.write, typeof(CategoryEntity));
+
+            List<AttributeEntity> attributes = []; 
+
+            foreach (var attr in dto.Attributes)
+            {
+                attributes.Add(AttributeEntity.Create(attr.Name, attr.Value, attr.Label)); 
+            }
+
+            var tags = JsonConvert.SerializeObject(dto.Tags); 
+
+            var category = CategoryEntity.Create(dto.Name, tags, dto.Description, attributes);
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return category;
         }
     }
 }
